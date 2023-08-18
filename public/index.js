671726ce0f2b7672082595cbaaf02219f77d740c
Your Name <sebastian.bjelkstam@gmail.com>
2023-08-18 13:30:57 +0200

    Chat-app

---
+++ public/index.js
@@ -0,0 +1,56 @@
+const socket = io({ autoConnect: false });
+
+let sendToTheRoom = document.querySelector(".sendToTheRoom");
+let theList = document.querySelector(".theList");
+let theListRoom = document.querySelector(".theListRoom");
+
+let sendInTheRoom = () => {
+  socket.connect();
+  let writeName = document.querySelector(".writeName").value;
+
+  let room = document.querySelector(".writeRoom").value;
+
+  console.log(writeName);
+  console.log(room);
+
+  //   writeName.value = "";
+
+  socket.emit("user_connect", writeName);
+
+  socket.emit("join_room", `${room}`);
+
+  socket.on("a_user_has_connect", (username) => {
+    let li = document.createElement("li");
+    li.innerText = username + " join the chat!";
+    theList.appendChild(li);
+  });
+
+  socket.emit("message", () => {
+    let theLi = document.createElement("li");
+    theLi.innerText = room;
+    theListRoom.appendChild(theLi);
+  });
+};
+
+sendToTheRoom.addEventListener("click", sendInTheRoom);
+
+let sendMessage = document.querySelector(".sendMessage");
+let outputMessage = document.querySelector(".output-message");
+
+let send = () => {
+  let inputChat = document.querySelector(".inputChat").value;
+
+  socket.on("message", (msg) => {
+    let li = document.createElement("li");
+    li.innerText = msg + " join the room!";
+    theList.appendChild(li);
+
+    // let li = document.createElement("li");
+    // li.innerText = msg + " join the chat!";
+    // outputMessage.appendChild(li);
+  });
+
+  console.log(inputChat);
+};
+
+sendMessage.addEventListener("click", send);
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("A user has connect: ", socket.id);
+  socket.on("user_connect", (username) => {
+    io.emit("a_user_has_connect", username);
+    console.log(`${username} har joinat!`);
+
+    socket.on("join_room", (room) => {
+      socket.join(`${username} join ${room}`);
+      console.log(io.sockets.adapter.rooms);
+    });
+  });
+
+  //   socket.on("join_room", (room) => {
+  //     socket.join(`${us}`room);
+  //     console.log(io.sockets.adapter.rooms);
+  //   });
+
+  //   socket.on("leave1", (room) => {
+  //     socket.leave(room);
+  //     console.log(io.sockets.adapter.rooms);
+  //   });
+
+  socket.on("message", (msg) => {
+    io.to("join_room").emit("message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnect");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Servern är igång");
+});
